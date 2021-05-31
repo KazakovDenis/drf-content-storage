6552467d2098452e33855715d312ce7f0a952881
Denis Kazakov <kazakovdeyu@yandex.ru>
2021-05-31 08:35:09 +0300

    added db wait in CI

--- content_storage/core/urls.py
+++ content_storage/core/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+
 from django.urls import path, include
 
+from core.views import index_view
+
 urlpatterns = [
+    path('', index_view, name='index'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls', namespace='api')),
